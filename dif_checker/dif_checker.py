e7ca019b76b380572167f4e5b0d25349b2875f30
Samuel-Scalbert <sam.scalbert@gmail.com>
2024-03-05 15:27:18 +0100

    final commit

--- dif_checker/dif_checker.py
+++ dif_checker/dif_checker.py
@@ -1,48 +1,66 @@
 import xml.etree.ElementTree as ET
-import logging
 import json
-import sys
-import string
 from .testt2 import wizzard_xml_json2
-
-logging.basicConfig(filename='dif_checker.log', encoding='utf-8', level=logging.DEBUG, filemode='w', format='%(levelname)s-%(message)s')
 
 
 def xml_enhance_json(xml_path, json_path):
 
-    with open(xml_path, 'r', encoding='utf-8') as xml_file:
-        root = ET.parse(xml_file)
-        root = root.getroot()
+    with open(xml_path, 'r') as xml_file:
+        tree = ET.parse(xml_file)
+        root = tree.getroot()
         ns = {"tei": "http://www.tei-c.org/ns/1.0"}
-
 
     with open(json_path, 'r') as json_file:
         data_json = json.load(json_file)
         data_json_get_mentions = data_json.get("mentions")
-        index = -1
-        div_elements = root.findall("./tei:text/tei:body//tei:div", ns)
+        software_types = {}
+        software_counts = {}
+
+        for mention in data_json_get_mentions:
+            software_type = mention["software-type"]
+            software = mention["software-name"]["normalizedForm"]
+
+            # Count occurrences of software types
+            if software_type in software_types:
+                software_types[software_type] += 1
+            else:
+                software_types[software_type] = 1
+
+            # Count occurrences of specific software names
+            if software in software_counts:
+                software_counts[software] += 1
+            else:
+                software_counts[software] = 1
+
+        print(software_types)
+        print(software_counts)
+
+    paths = [
+        "./tei:teiHeader/tei:profileDesc/tei:abstract/tei:div",
+        "./tei:text/tei:body/tei:div",
+        "./tei:text/tei:back/tei:div[@type='acknowledgement']/tei:div",
+        "./tei:text/tei:back/tei:div[@type='availability']/tei:div",
+        "./tei:text/tei:back/tei:div[@type='annex']/tei:div"
+        ]
+
+    paths = ["./tei:text/tei:body/tei:div"]
+    context_list_found = []
+    for path in paths:
+        div_elements = root.findall(path, ns)
         for div_element in div_elements:
             p_elements = div_element.findall(".//tei:p", ns)
             for p in p_elements:
                 result = wizzard_xml_json2(p, data_json_get_mentions)
-        '''with open(json_path, 'r') as json_file:
-        data_json = json.load(json_file)
-        data_json_get_mentions = data_json.get("mentions")
-        index = -1
-        div_elements = root.findall("./tei:text/tei:body//tei:div", ns)
-        for div_element in div_elements:
-            p_elements = div_element.findall(".//tei:p", ns)
-            for p in p_elements:
-                for software_mention in data_json_get_mentions:
-                    index += 1
-                    if software_mention["software-type"] == "software":
-                        software_name = software_mention["software-name"]["rawForm"]
-                        software_name = software_name.replace("\n", " ")
-                        software_context = software_mention["context"]
-                        offset_start = software_mention["software-name"]["offsetStart"]
-                        offset_end = software_mention["software-name"]["offsetEnd"]
-                        result = wizzard_xml_json(software_context, offset_start,offset_end,software_name, p)
+                if result == False:
+                    pass
+                else:
+                    modified_p, context_list_found_in_p = result
+                    if len(context_list_found_in_p) > 0:
+                        context_list_found += context_list_found_in_p
+    print(f'we founded : {len(context_list_found)} context')
 
-        modified_xml = ET.tostring(root, encoding='unicode')
-        with open("./test.xml", 'w', encoding='utf-8') as output_xml_file:
-            output_xml_file.write(modified_xml)'''
+    # Write the modified XML back to the file
+    with open("./test_monstre.xml", 'wb') as output_xml_file:
+        ET.register_namespace("", "http://www.tei-c.org/ns/1.0")
+        xml_str = ET.tostring(root, encoding='utf-8')
+        output_xml_file.write(xml_str)
--- dif_checker/testt2.py
+++ dif_checker/testt2.py
@@ -6,6 +6,7 @@ from .test import longest_common_substrings,find_closest_number,contains_special
 def wizzard_xml_json2(p, software_mentions):
     p_string = "".join(p.itertext())
     original_sub_tags_list = []
+    context_list_found = []
     ET.register_namespace('', "http://www.tei-c.org/ns/1.0")
     if p_string == None:
         return None
@@ -25,9 +26,8 @@ def wizzard_xml_json2(p, software_mentions):
                 attributes_dict = None
                 original_sub_tags_list.append([elm.tag, elm.text, elm.tail, index, "sub-element", attributes_dict])
     full_list_software = []
-
     for software_mention in software_mentions:
-        if software_mention["software-type"] == "software":
+        if software_mention["software-type"] in ["software","implicit","component"]:
             software = software_mention["software-name"]["rawForm"]
             context = software_mention["context"]
             if software.find('\n') != -1:
@@ -37,46 +37,69 @@ def wizzard_xml_json2(p, software_mentions):
                 offsetStart = software_mention["software-name"]["offsetStart"]
         index_context = p_string.find(context)
         normal_found = -1
+        cleaned_found = -1
         if index_context != -1:
             normal_found = 1
             offsetStart_full_str = index_context + offsetStart
             if p_string[offsetStart_full_str: offsetStart_full_str + len(software)] == software:
                 software_list = ['software', software, None, offsetStart_full_str, 'software', None, 'normal']
                 full_list_software.append(software_list)
+                context_list_found.append(context)
             else:
                 print(f'error index (normal) software we found {p_string[offsetStart_full_str: offsetStart_full_str+len(software)]}')
-        if fuzz.partial_ratio(p_string, context) >= 95 and normal_found != 1 and len(p_string) >= len(context):
+        characters = ['-\n','\n']
+        for special_character in characters:
+            cleaned_context = context.replace(special_character, '')
+            if context.find(special_character) != -1 and p_string.find(cleaned_context) != -1:
+                cleaned_found = 1
+                p_cleaned_index = p_string.find(cleaned_context)
+                index_n = context.find(special_character)
+                occurrences = find_occurrences(software, p_string[p_cleaned_index:])
+                p_software_index = find_closest_number(offsetStart, occurrences)
+                if p_string[p_cleaned_index + p_software_index:p_cleaned_index+p_software_index+len(software)] == software:
+                    offsetStart_full_str = p_cleaned_index + p_software_index
+                    software_list = ['software', software, None, offsetStart_full_str, 'software', None, 'cleaned']
+                    full_list_software.append(software_list)
+                    context_list_found.append(context)
+                else:
+                    print(f'error index (cleaned) software for: {context}')
+        if fuzz.partial_ratio(p_string, context) >= 95 and normal_found != 1 and cleaned_found != 1 and len(p_string) >= len(context):
             index_context_from_fuzzy = longest_common_substrings(p_string, context)
-            occurences = find_occurrences(software,p_string[index_context_from_fuzzy:])
-            print('la:',occurences,offsetStart)
-            p_software_index = find_closest_number(offsetStart,occurences)
+            occurrences = find_occurrences(software,p_string[index_context_from_fuzzy:])
+            p_software_index = find_closest_number(offsetStart,occurrences)
             if p_string[index_context_from_fuzzy + p_software_index:index_context_from_fuzzy + p_software_index+len(software)] == software:
                 offsetStart_full_str = index_context_from_fuzzy + p_software_index
                 software_list = ['software', software, None, offsetStart_full_str, 'software', None, 'fuzzy']
                 full_list_software.append(software_list)
+                context_list_found.append(context)
             else:
                 print(f'error index (fuzzy) software for: {context}')
-    for elm in full_list_software:
-        print(elm)
     if not full_list_software:
         return False
-
     list_len = len(full_list_software) + len(original_sub_tags_list)
     full_list_software = sorted(full_list_software, key=lambda x: x[3])
+    if len(original_sub_tags_list) == 0:
+        tag_name, software, tail, index, type, attr, tech_found = full_list_software[0]
+        tail_software = p_string[index + len(software):]
+        old_string = p.text
+        new_p_text = p_string[:index]
+        if new_p_text + software + tail_software == old_string:
+            p.text = new_p_text
+            software_list = ['software', software, tail_software, index, 'force_software', None]
+            original_sub_tags_list.append(software_list)
+            full_list_software.pop(0)
+            print(f'{software} was added to the empty list')
     for tags in full_list_software:
         str_alt_index = tags[3]
         software = tags[1]
         nb = 0
         for elm in range(len(original_sub_tags_list)):
-            if str_alt_index < original_sub_tags_list[nb][3] and nb == 0:
+            if (str_alt_index < original_sub_tags_list[nb][3] and nb == 0):
                 old_string = p.text
                 new_p_text = p_string[:str_alt_index]
                 tail_software = p_string[str_alt_index + len(software):original_sub_tags_list[nb][3]]
                 if len(tail_software) == 0:
                     tail_software = ' '
-                if (str_alt_index > original_sub_tags_list[nb][3] or str_alt_index == original_sub_tags_list[nb][3]) and str_alt_index < original_sub_tags_list[nb][3] + len(original_sub_tags_list[nb][1]):
-                    print('inside text', software)
-                    break
                 if new_p_text + software + tail_software == old_string:
                     p.text = new_p_text
                     software_list = ['software', software, tail_software, str_alt_index, 'software', None]
@@ -85,7 +108,6 @@ def wizzard_xml_json2(p, software_mentions):
                     original_sub_tags_list = sorted(original_sub_tags_list, key=lambda x: x[3])
                     break
                 else:
-                    #print(f'old_p_string : "{old_string}"\n new_p_text : "{new_p_text}"\n new_software_tail : "{tail_software}"')
                     print(f'CRITICAL : {software}(start) : CRITICAL')
             try:
                 if str_alt_index >= original_sub_tags_list[nb][3] and str_alt_index < original_sub_tags_list[nb+1][3]:
@@ -106,7 +128,6 @@ def wizzard_xml_json2(p, software_mentions):
                             print(f'{software} was added to the list (middle) as a sub-child')
                             break
                         else:
-                            # print(f'old_p_string : "{old_string}"\n new_ref_tail : "{original_sub_tags_list[len(original_sub_tags_list)-1][2]}"\n new_software_tail : "{tail_software}"')
                             print(f'CRITICAL : {software}(middle \ inside) : CRITICAL')
                         break
                     if new_tail_prior_tag + software + tail_software == old_string:
@@ -117,8 +138,6 @@ def wizzard_xml_json2(p, software_mentions):
                         original_sub_tags_list = sorted(original_sub_tags_list, key=lambda x: x[3])
                         break
                     else:
-                        #dif_string_checker(old_string,new_tail_prior_tag + software + tail_software)
-                        #print(f'old_p_string : "{old_string}"\n new_ref_tail : "{new_tail_prior_tag}"\n new_software_tail : "{tail_software}"')
                         print(f'CRITICAL : {software}(middle) {old_string} : CRITICAL')
             except IndexError:
                 if str_alt_index >= original_sub_tags_list[len(original_sub_tags_list)-1][3]:
@@ -138,7 +157,6 @@ def wizzard_xml_json2(p, software_mentions):
                             list_len -= 1
                             break
                         else:
-                            # print(f'old_p_string : "{old_string}"\n new_ref_tail : "{original_sub_tags_list[len(original_sub_tags_list)-1][2]}"\n new_software_tail : "{tail_software}"')
                             print(f'CRITICAL : {software}(end \ inside) : CRITICAL')
                         break
                     if new_tail_prior_tag + software + tail_software == old_string:
@@ -149,20 +167,37 @@ def wizzard_xml_json2(p, software_mentions):
                         original_sub_tags_list = sorted(original_sub_tags_list, key=lambda x: x[3])
                         break
                     else:
-                        #print(f'old_p_string : "{old_string}"\n new_ref_tail : "{original_sub_tags_list[len(original_sub_tags_list)-1][2]}"\n new_software_tail : "{tail_software}"')
                         print(f'CRITICAL : {software}(end) : CRITICAL')
             original_sub_tags_list = sorted(original_sub_tags_list, key=lambda x: x[3])
             nb += 1
-    if len(original_sub_tags_list) == list_len:
-        print(f'THE JOB IS DONE {len(original_sub_tags_list)}/{list_len}\n')
-
-
-    final_p_string = p.text
+    original_sub_tags_list = sorted(original_sub_tags_list, key=lambda x: x[3])
     for elm in original_sub_tags_list:
-        if elm[2] == None:
-            final_p_string += " "
-        if elm[1]:
-            final_p_string += elm[1]
-        if elm[2]:
-            final_p_string += elm[2]
-    #dif_string_checker(final_p_string, saved_p_string)
+        if elm[0] == 'software':
+            for elm in list(p):
+                p.remove(elm)
+            break
+        else:
+            pass
+    for elm in original_sub_tags_list:
+        if len(elm) >= 7:
+            tag_name, tag_content, tail, index, type, attr, child_software = elm
+        else:
+            tag_name, tag_content, tail, index, type, attr = elm
+            child_software = None
+
+        tag = ET.Element(tag_name)
+        tag.text = tag_content
+        tag.tail = tail
+        if attr != None:
+            for keys, values in attr.items():
+                tag.set(keys, values)
+
+        if child_software:
+            software_child = ET.Element(child_software[0])
+            software_child.text = child_software[1]
+            software_child.tail = child_software[2]
+            tag.insert(0, software_child)
+
+        p.append(tag)
+
+    return [p,context_list_found]
